cdf19a7778f7b0c90e4f44369cb6eb2fc1fb0bbb
SKYE-633 <52529600+SKYE-633@users.noreply.github.com>
2019-07-08 11:54:59 +0800

    Update practice-2.js

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -4,8 +4,8 @@ function collectSameElements(collectionA, collectionB) {
 int i;
 int j;
 for (var i = 0; i < collectionA.length; i++) {
-  for (var j = 0; j < (A.indexOf(collectionB[j]).length; j++) {
-    if (collectionA[i] == collectionB[j]){
+  for (var j = 0; j < (B.indexOf(collectionB[j]).length; j++) {
+    if (collectionA[i] == (B.indexOf(collectionB[j])){
       const result = collectSameElements(collectionA, collectionB);
       console.log(result);
       return '实现练习要求，并改写该行代码。';
