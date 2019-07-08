84629d7c8edb5e407420c2835098d4012ac60951
SKYE-633 <52529600+SKYE-633@users.noreply.github.com>
2019-07-08 11:40:10 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+int i;
+int j;
+for (var i = 0; i < collectionA.length; i++) {
+  for (var j = 0; j < collectionB.length; j++) {
+    if (collectionA[i] == collectionB[j]){
+      const result = collectSameElements(collectionA, collectionB);
+      console.log(result);
+      return '实现练习要求，并改写该行代码。';
+    }
+  }
+}
 }
