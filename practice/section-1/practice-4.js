5be88121aeee4c6cdd5398ca9fdcaded4f6db916
SKYE-633 <52529600+SKYE-633@users.noreply.github.com>
2019-07-08 11:49:57 +0800

    Update practice-4.js

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+    var tempb = object_b.value.toString().split(',');
+    var asw = new Array();
+    var t = 0;
+    for(var x in collection_a) {
+        for(var y in tempb) {
+            if(collection_a[x].key == tempb[y]) {
+                asw[t] = collection_a[x].key;
+                t++
+            }
+        }
+    }
+    return '实现练习要求，并改写该行代码。';
 }
+ 
